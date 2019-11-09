84cc34c39bdece0aec502dee0ec2d5b405d3d68f
sinantok <sinantok@outlook.com>
2019-11-09 21:34:21 +0300

    login service added

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using CoreWebApi.Models.Identification;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CoreWebApi.Controllers
 {
@@ -29,6 +33,11 @@ namespace CoreWebApi.Controllers
         [Route("Register")]
         public async Task<Object> PostApplicationUser(ApplicationUserModel model)
         {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if(user != null)
+            {
+                return BadRequest(new { message = "username already registered." });
+            }
             model.Role = "Admin";
             var applicationUser = new ApplicationUser()
             {
@@ -47,6 +56,42 @@ namespace CoreWebApi.Controllers
             {
 
                 throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            try
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    //Get role assigned to the user
+                    var role = await _userManager.GetRolesAsync(user);
+                    IdentityOptions _options = new IdentityOptions();
+
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new Claim[]
+                        {
+                        new Claim("UserID",user.Id.ToString()),
+                        new Claim(_options.ClaimsIdentity.RoleClaimType,role.FirstOrDefault())
+                        }),
+                        Expires = DateTime.UtcNow.AddDays(1),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_applicationSettings.JWT_Secret)), SecurityAlgorithms.HmacSha256Signature)
+                    };
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                    var token = tokenHandler.WriteToken(securityToken);
+                    return Ok(new { token });
+                }
+                return BadRequest(new { message = "Username or password is incorrect." });
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
     }
